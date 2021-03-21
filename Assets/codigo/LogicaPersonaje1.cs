8874dd02be58b20308569b2ae56db9bc3fac27d4
Mariana Casella <programaciondeverdad@gmail.com>
2021-03-21 11:06:05 -0300

    Seminario Juego Unity 3d

---
+++ Assets/codigo/LogicaPersonaje1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje1 : MonoBehaviour
+{
+	public float velocidad_movimiento = 5.0f;
+	public float velocidad_rotacion = 200.0f;
+	private Animator anim;
+	public float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	anim = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * velocidad_rotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidad_movimiento);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
